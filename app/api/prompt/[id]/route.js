e48da8c24a5a40e43293660d40802564016e4f9f
tdp <dinhphuongthaoqia@gmail.com>
2023-11-25 02:02:06 +0700

    Profile page with prompt id update and delete

---
+++ app/api/prompt/[id]/route.js
@@ -0,0 +1,55 @@
+import Prompt from "@models/prompt";
+const { connectToDB } = require("@utils/database");
+
+// GET read
+export const GET = async (req, { params }) => {
+  try {
+    await connectToDB();
+    const prompt = await Prompt.findById(params.id).populate("creator");
+    if (!prompt) {
+      return new Response("Prompt not found", { status: 404 });
+    }
+    return new Response(JSON.stringify(prompt), {
+      status: 200,
+    });
+  } catch (error) {
+    return new Response("Failed to fetch prompt", { status: 500 });
+  }
+};
+
+// PATCH update
+export const PATCH = async (req, { params }) => {
+  const { prompt, tag } = await req.json();
+  try {
+    await connectToDB();
+    console.log(1, params.id);
+
+    const existingPrompt = await Prompt.findById(params.id);
+    if (!existingPrompt) {
+      return new Response("Prompt not found", { status: 404 });
+    }
+    existingPrompt.prompt = prompt;
+    existingPrompt.tag = tag;
+    await existingPrompt.save();
+    return new Response(JSON.stringify(existingPrompt), {
+      status: 200,
+    });
+  } catch (error) {
+    return new Response("Failed to update prompt", { status: 500 });
+  }
+};
+// DELETE delete
+export const DELETE = async (req, { params }) => {
+  try {
+    await connectToDB();
+    const deletedPrompt = await Prompt.findByIdAndDelete(params.id);
+    if (!deletedPrompt) {
+      return new Response("Prompt not found", { status: 404 });
+    }
+
+    return new Response("Prompt deleted successfully", { status: 200 });
+  } catch (error) {
+    console.error("Error deleting prompt:", error);
+    return new Response("Error deleting prompt", { status: 500 });
+  }
+};
