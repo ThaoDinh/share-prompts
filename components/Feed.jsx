6c89695146a336a28df5786fc23b989e27a7bdb0
tdp <dinhphuongthaoqia@gmail.com>
2023-11-28 23:24:16 +0700

    Thêm search feature

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,5 +1,6 @@
 "use client";
 import { useEffect, useState } from "react";
+import { debounce } from "lodash";
 import PromptCard from "./PromptCard";
 
 const PromptCardsList = ({ data, handleTagClick }) => {
@@ -17,11 +18,10 @@ const PromptCardsList = ({ data, handleTagClick }) => {
 };
 
 function Feed() {
-  const [searchText, setSearchText] = useState("");
+  const [searchedResults, setSearchedResults] = useState([]);
   const [posts, setPosts] = useState([]);
-  const handleSearchChange = (e) => {
-    setSearchText(e.target.value);
-  };
+  const [searchText, setSearchText] = useState("");
+
   useEffect(() => {
     const fetchPosts = async () => {
       const response = await fetch("/api/prompt");
@@ -30,6 +30,26 @@ function Feed() {
     };
     fetchPosts();
   }, []);
+
+  const handleTagClick = (postTag) => {
+    setSearchText(postTag);
+    debounceOnChange(postTag);
+  };
+  const handleSearchChange = (e) => {
+    setSearchText(e.target.value);
+    debounceOnChange(e.target.value);
+  };
+  const debounceOnChange = debounce((searchValue) => {
+    const matchedPosts = posts.filter((post) => {
+      const regex = new RegExp(searchValue, "gi");
+      return (
+        post.prompt.match(regex) ||
+        post.tag.match(regex) ||
+        post.creator.username.match(regex)
+      );
+    });
+    setSearchedResults(matchedPosts);
+  }, 500);
 
   return (
     <section className="feed">
@@ -43,7 +63,14 @@ function Feed() {
           required
         />
       </form>
-      <PromptCardsList data={posts} handleTagClick={() => {}} />
+      {searchText ? (
+        <PromptCardsList
+          data={searchedResults}
+          handleTagClick={handleTagClick}
+        />
+      ) : (
+        <PromptCardsList data={posts} handleTagClick={handleTagClick} />
+      )}
     </section>
   );
 }
